4c3dfda079175d0d6a17f768b927e40b2e40e2f6
SergWebAcceptance <serg.web@acceptance.ltd>
2025-04-07 13:50:15 +0300

    initial commit

---
+++ src/shared/ui/components/footer/footer.tsx
@@ -0,0 +1,6 @@
+"use client";
+import st from "./footer.module.scss";
+
+export default function Footer() {
+  return <footer className={st.footer}>Footer</footer>;
+}
---
+++ src/shared/ui/components/header/header.tsx
@@ -0,0 +1,6 @@
+"use client";
+import st from "./header.module.scss";
+
+export default function Header() {
+  return <header className={st.header}>Header</header>;
+}
