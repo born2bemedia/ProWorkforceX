762eaa59135bacc79572b5614de0040bf173e0f4
daniil-b2be <dan.developer@born2be.media>
2025-04-30 13:47:34 +0300

    contact us meta

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,4 +1,17 @@
+import type { Metadata } from 'next';
+
 import { ContactForm, ContactInfo, Hero, SocialMedia } from './components';
+
+export const metadata: Metadata = {
+  title: 'Contact ProWorkforceX - Get in Touch for HR Solutions',
+  description:
+    'Reach out to ProWorkforceX for personalized HR solutions. Whether you need advice or support or want to get started with our services, we’re here to help.',
+  openGraph: {
+    title: 'Contact ProWorkforceX - Get in Touch for HR Solutions',
+    description:
+      'Reach out to ProWorkforceX for personalized HR solutions. Whether you need advice or support or want to get started with our services, we’re here to help.',
+  },
+};
 
 export default function ContactUs() {
   return (
